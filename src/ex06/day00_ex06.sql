7aeb9a4caaac9fc13377e9553554cfefbb9da065
bonnyped <bonnyped@student.21-school.ru>
2023-10-20 22:57:58 +0600

    ex6

---
+++ src/ex06/day00_ex06.sql
@@ -0,0 +1,15 @@
+SELECT
+        (SELECT person.name
+        FROM person
+        WHERE po.person_id = person.id) AS NAME,
+       (SELECT
+               CASE
+                    WHEN person.name like 'Denis' THEN 'True'
+                    ELSE 'False'
+               END AS check_name
+        FROM person
+        WHERE po.person_id = person.id)
+
+FROM person_order AS po
+WHERE po.order_date = '2022-01-07'
+    AND (po.menu_id = 13 OR po.menu_id = 14 OR po.menu_id = 18);
